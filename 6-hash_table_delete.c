649538a93e55d6940b08eae326684f79a1a7ae02
Afnan9999 <afnan-911@outlook.sa>
2025-11-01 20:51:59 +0300

    Update 6-hash_table_delete.c

--- 6-hash_table_delete.c
+++ 6-hash_table_delete.c
@@ -1,1 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: Pointer to the hash table
+ *
+ * Description: Prints key/value pairs in the order they appear in the array.
+ * Format: {'key1': 'value1', 'key2': 'value2', ...}
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int i;
+    hash_node_t *node;
+    int first = 1;
+
+    if (ht == NULL)
+        return;
+
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+        node = ht->array[i];
+        while (node != NULL)
+        {
+            if (first == 0)
+                printf(", ");
+            printf("'%s': '%s'", node->key, node->value);
+            first = 0;
+            node = node->next;
+        }
+    }
+    printf("}\n");
+}
 
